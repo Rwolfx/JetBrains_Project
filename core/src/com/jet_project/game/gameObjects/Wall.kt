6c27b1a9b19e2a642ed8c5de6d7793dc1d64cd62
Raul <rwolf2001@gmail.com>
2017-11-19 22:55:33 +0300

    Physics bug fix

--- core/src/com/jet_project/game/gameObjects/Wall.kt
+++ core/src/com/jet_project/game/gameObjects/Wall.kt
@@ -10,7 +10,7 @@ import com.jet_project.game.game.GameWorld
 class Wall(name: String, currentWorld: GameWorld, x: Float, y: Float) : GameObject(name, currentWorld, x, y) {
 
     override fun init() {
-        img = Texture("badlogic.jpg")
+        img = Texture("gameObjects/wall.png")
         sprite = Sprite(img)
         sprite.setPosition(positionX,positionY)
         super.init()
